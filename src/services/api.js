ff66c5bcb0b506ad39e1df0856f01c27bcafa20b
gladsonramos <50427216+gladsonramos@users.noreply.github.com>
2022-11-26 19:00:43 -0300

    alteraçaoBackEnd

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-    baseURL: "https://back-end-robos.herokuapp.com",   
+    baseURL: "https://robos.onrender.com",   
     
 })
 //Endpoints User
